73f2e08c7832680ea722524786d39a32922691ea
natchamon inphluek <62160059@go.buu.ac.th>
2022-03-29 15:59:00 +0700

    page login update nonsuccess

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,20 +5,20 @@
   <div class="col-md-6 offset-md-3" style="border: black">
     <div>
     <div>
-      <h3>Login</h3>
+      <center><h3>BUU E-Booking</h3></center>
       <hr />
     </div>
     <form action="">
       <div class="from-group">
-        <label>Username</label>
-        <input type="text" class="form-control" />
-      </div>
+        <!-- <label>Username</label> -->
+        <input type="text" class="form-control" placeholder="Username">
+      </div> <br>
       <div class="from-group">
-        <label>Password</label>
-        <input type="password" class="form-control" />
+        <!-- <label>Password</label> -->
+        <input type="password" class="form-control" placeholder="Password" >
       </div>
       <div class="my-3">
-        <button type="submit" class="btn btn-primary">
+        <button type="submit" class="btn btn-primary" style="width:100%">
           Login
         </button>
       </div>

c9ada72619d56badb39de7c551760541d48de600
natchamon inphluek <62160059@go.buu.ac.th>
2022-04-13 19:58:00 +0700

    router serachroom

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="Menubar">
-    <button class="button" v-if="isUser" >
+    <button class="button" v-if="isUser" @click="$router.push('/')">
       <i class="fa fa-search" style="font-size: 30px"></i>&nbsp; ค้นหาห้อง
     </button>
-
     <button class="button" v-if="isUser">
       <i class="fa fa-calendar" style="font-size: 30px"></i>&nbsp; การจอง
     </button>

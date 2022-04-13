0acb319dbccbffc0d6a95e8dc9a20744ebc8cc67
Teerarak Techarat <62160273@go.buu.ac.th>
2022-04-14 00:22:30 +0700

    finish Agency main page

---
+++ src/views/ManageAgency.vue
@@ -0,0 +1,75 @@
+<template>
+  <Auth>
+    <div class="home">
+      <center> <h3 style="padding-top: 2%">จัดการหน่วยงาน</h3></center>
+      <br>
+      <div style="padding-left: 1%">
+  <b-form inline>
+    <label class="sr-only" for="inline-form-input-BuildingCode">Name</label>
+    <b-form-input
+      id="inline-form-input-BuildingCode"
+      class="mb-2 mr-sm-2 mb-sm-0"
+      placeholder="ค้นหา"
+      style="width: 25% "
+    ></b-form-input>
+    <b-button>SEARCH</b-button>&nbsp;&nbsp;&nbsp;
+       <b-button variant="success">เพิ่มข้อมูล</b-button>
+  </b-form>
+      </div>
+    </div>
+    <br>
+    <div class="background-search">
+    <b-container fluid>
+      <b-row>
+      </b-row>
+      <b-row>
+        <b-col>
+          <b-table :items="productItems" :fields="fields">
+            <template #cell(operators)>
+              <b-button variant="warning">แก้ไข</b-button
+              ><b-button
+                class="ml-3"
+                variant="danger"
+                >ลบ</b-button
+              >
+            </template>
+          </b-table>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+  </Auth>
+</template>
+
+<script>
+import Auth from '../components/Auth.vue'
+export default {
+  BuildingCode: 'Home',
+  components: {
+    Auth
+  },
+  data () {
+    return {
+      fields: [
+        { key: 'AgencyId', label: 'ไอดี' },
+        { key: 'AgencyName', label: 'หน่วยงาน' },
+        { key: 'operators', label: 'การจัดการ' }
+      ],
+      productItems: [
+        { AgencyId: 1, AgencyName: 'วิทยาการสารสนเทศ' }
+
+      ]
+    }
+  }
+}
+</script>
+<style>
+.background-search {
+  background-color: gray;
+  height: 100%;
+  width: 100%;
+}
+.position-buttonSelected {
+  margin-left: 100%;
+}
+</style>
--- src/views/Petition.vue
+++ src/views/Petition.vue
@@ -1,8 +1,11 @@
 <template>
-<div>
-<div class="row" style="padding-top:3cm">
-
-  <div class="col-md-6 offset-md-3" style="border: black">
-    <div>
-    <div>
+  <div>
+    <div class="row" style="padding-top: 3cm">
+      <div class="col-md-6 offset-md-3" style="border: black">
+        <div>
+          <div></div>
+        </div>
+      </div>
     </div>
+  </div>
+</template>
